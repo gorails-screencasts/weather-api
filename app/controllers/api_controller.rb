b675073901b9fb659ceafe772f23f596f6e55c88
Chris Oliver <excid3@gmail.com>
2017-01-06 20:00:18 -0600

    Add Devise strategy for authentication

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,24 +2,11 @@ class ApiController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   before_action :set_default_format
-  before_action :authenticate_token!
+  before_action :authenticate_user!
 
   private
 
     def set_default_format
       request.format = :json
-    end
-
-    def authenticate_token!
-      payload = JsonWebToken.decode(auth_token)
-      @current_user = User.find(payload["sub"])
-    rescue JWT::ExpiredSignature
-      render json: {errors: ["Auth token has expired"]}, status: :unauthorized
-    rescue JWT::DecodeError
-      render json: {errors: ["Invalid auth token"]}, status: :unauthorized
-    end
-
-    def auth_token
-      @auth_token ||= request.headers.fetch("Authorization", "").split(" ").last
     end
 end
